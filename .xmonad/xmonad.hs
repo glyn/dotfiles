99d01cdcccff0de9d74206cb7924a573449f483d
Glyn Normington <glyn.normington@gmail.com>
2022-12-22 01:51:20 +0000

    Revert "Make sure keyboard layout stays at uk"
    The change does not work as-is. Need to configure X properly.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,6 +5,8 @@ import XMonad.Hooks.ManageDocks
 main = do
    xmproc <- spawnPipe "xmobar /home/glyn/.xmonad/xmobar.config"
 
+   xmproc <- spawnPipe "setxkbmap -layout gb"
+
    xmonad $ docks $ def
      { layoutHook = avoidStruts $ layoutHook defaultConfig
      , terminal = "xterm -e /bin/zsh"
